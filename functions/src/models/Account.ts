67adc8dc3a0f3b2591b6fb75d5e1ae69f9bb1647
Kaden Rice <kadenrice13@gmail.com>
2024-02-07 09:37:57 -0800

    updated account model

--- functions/src/models/Account.ts
+++ functions/src/models/Account.ts
@@ -8,4 +8,28 @@ export default interface Account {
   displayName: string;
   email: string;
   photoURL?: string;
+
+  // Carbon Emissions
+  totalCarbonEmission?: number;
+  totalCarbonUnit?: string;
+  activities?: Activity[];
+}
+
+interface Activity {
+  uuid: string;
+  name: string;
+
+  // electricity, flight, shipping, vehicle, fuel combustion, etc etc..
+  typeOfCarbon: string;
+  typeUnit: string;
+  typeAmt: string;
+  carbon_g: number;
+  carbon_lb: number;
+  carbon_kg: number;
+  carbon_mt: number;
+
+  // for more information
+  country?: string;
+  state?: string;
+  estimatedDate?: string;
 }
